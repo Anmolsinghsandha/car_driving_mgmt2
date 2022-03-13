7a25ff9b5abeb32f51395b6609025ebd90f488d8
Manpreet <manpreet.7385@gmail.com>
2022-03-13 12:11:17 +0530

    pushed

---
+++ includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("bipsytn1ifmip5bqaggu-mysql.services.clever-cloud.com", "uobsyigx9guohfpx", "BCqjjUceB0K3B5yFPHMV", "bipsytn1ifmip5bqaggu");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
